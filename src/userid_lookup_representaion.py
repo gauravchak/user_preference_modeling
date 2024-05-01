912b7882116709c2896aff52595809f1adadb81c
Gaurav Chakravorty <gchak207@gmail.com>
2024-05-01 18:32:40 +0000

    changed the id lookup class from multi task estimator to userid_lookup_representation.py

---
+++ src/userid_lookup_representaion.py
@@ -0,0 +1,60 @@
+from typing import List
+
+import torch
+import torch.nn as nn
+
+from src.multi_task_estimator import MultiTaskEstimator
+
+
+class UseridLookupRepresentation(MultiTaskEstimator):
+    """
+    This uses embedding table lookup
+    """
+
+    def __init__(
+        self,
+        num_tasks: int,
+        user_id_hash_size: int,
+        user_id_embedding_dim: int,
+        user_features_size: int,
+        item_id_hash_size: int,
+        item_id_embedding_dim: int,
+        item_features_size: int,
+        cross_features_size: int,
+        user_value_weights: List[float],
+    ) -> None:
+        """
+        params:
+            num_tasks (T): The tasks to compute estimates of
+            user_id_hash_size: the size of the embedding table for users
+            user_id_embedding_dim (DU): internal dimension
+            user_features_size (IU): input feature size for users
+            item_id_hash_size: the size of the embedding table for items
+            item_id_embedding_dim (DI): internal dimension
+            item_features_size: (II) input feature size for items
+            cross_features_size: (IC) size of cross features
+            user_value_weights: T dimensional weights, such that a linear
+            combination of point-wise immediate rewards is the best predictor
+            of long term user satisfaction.
+        """
+        super(UseridLookupRepresentation, self).__init__(
+            num_tasks=num_tasks,
+            user_id_embedding_dim=user_id_embedding_dim,
+            user_features_size=user_features_size,
+            item_id_hash_size=item_id_hash_size,
+            item_id_embedding_dim=item_id_embedding_dim,
+            item_features_size=item_features_size,
+            cross_features_size=cross_features_size,
+            user_value_weights=user_value_weights,
+        )
+        self.user_id_embedding_arch = nn.Embedding(
+            user_id_hash_size, user_id_embedding_dim)
+
+    def get_user_embedding(
+        self,
+        user_id: torch.Tensor,  # [B]
+        user_features: torch.Tensor,  # [B, IU]
+    ) -> torch.Tensor:
+        user_id_embedding = self.user_id_embedding_arch(user_id)
+        return user_id_embedding
+
